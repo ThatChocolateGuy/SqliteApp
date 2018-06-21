b35e2df0fcf2f16e38ecc476f88ed79da7bdd94c
Nem <nemx14@gmail.com>
2018-06-21 11:37:45 -0400

    Refresh

---
+++ app/src/main/java/com/example/tsuka/sqliteapp/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.tsuka.sqliteapp;
+
+import android.database.sqlite.SQLiteDatabase;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    DatabaseHelper myDb;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        myDb = new DatabaseHelper(this);
+    }
+}
