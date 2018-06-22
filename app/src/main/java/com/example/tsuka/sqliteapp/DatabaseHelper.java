5ff07e6fd63f9b475b216a8c38066a1512e06e0c
Nem <nemx14@gmail.com>
2018-06-21 21:29:16 -0400

    added ability to input text and validate db population

--- app/src/main/java/com/example/tsuka/sqliteapp/DatabaseHelper.java
+++ app/src/main/java/com/example/tsuka/sqliteapp/DatabaseHelper.java
@@ -1,8 +1,10 @@
 package com.example.tsuka.sqliteapp;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.view.Surface;
 
 public class DatabaseHelper extends SQLiteOpenHelper {
     public static final String DATABASE_NAME = "student.db";
@@ -14,7 +16,6 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 
     public DatabaseHelper(Context context) {
         super(context, DATABASE_NAME, null, 1);
-        SQLiteDatabase db = this.getWritableDatabase();
     }
 
     @Override
@@ -30,5 +31,19 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     public void onUpgrade(SQLiteDatabase db, int i, int i1) {
         db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
         onCreate(db);
+    }
+
+    public boolean insertData (String name, String surname, String marks) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+
+        contentValues.put(COL_2, name);
+        contentValues.put(COL_3, surname);
+        contentValues.put(COL_4, marks);
+
+        long result = db.insert(TABLE_NAME, null, contentValues);
+
+        if (result == -1) return false;
+        else return true;
     }
 }
--- app/src/main/java/com/example/tsuka/sqliteapp/MainActivity.java
+++ app/src/main/java/com/example/tsuka/sqliteapp/MainActivity.java
@@ -3,14 +3,47 @@ package com.example.tsuka.sqliteapp;
 import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     DatabaseHelper myDb;
+    EditText editName, editSurname, editMarks;
+    ImageButton btnAddData;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         myDb = new DatabaseHelper(this);
+
+        editName = (EditText) findViewById(R.id.editTextName);
+        editSurname = (EditText) findViewById(R.id.editTextSurname);
+        editMarks = (EditText) findViewById(R.id.editTextMarks);
+        btnAddData = (ImageButton) findViewById(R.id.imageButtonAdd);
+
+        addData();
+    }
+
+    public void addData() {
+        btnAddData.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        boolean isInsserted = myDb.insertData(editName.getText().toString(),
+                                editSurname.getText().toString(),
+                                editMarks.getText().toString() );
+
+                        if (isInsserted == true)
+                            Toast.makeText(MainActivity.this, "Data Inserted", Toast.LENGTH_LONG).show();
+                        else
+                            Toast.makeText(MainActivity.this, "Data Not Inserted", Toast.LENGTH_LONG).show();
+                    }
+                }
+        );
     }
 }
